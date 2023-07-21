76aa10939e575fc268a10b452e5282cf2fbc1657
Mert-Gunes <91959456+Mert-Gunes@users.noreply.github.com>
2023-07-21 16:53:55 +0300

    treasure game added(not ready yet)

---
+++ treasure_game.py
@@ -0,0 +1,21 @@
+import random
+
+print("Welcome to the treasure hunt game!")
+
+row_1 = ["◻️","◻️","◻️"]
+row_2 = ["◻️","◻️","◻️"]
+row_3 = ["◻️","◻️","◻️"]
+print("",row_1,"\n", row_2,"\n",row_3)
+
+map = [row_1, row_2, row_3]
+
+random1 = random.randint( 0 , 2)
+random2 = random.randint( 0 , 2)
+column = map[random1]
+column[random2] = "X"
+
+
+guess = str(input("Please enter a column and a row and sepreate them with comma."))
+
+split_guess = guess.split(",")
+if split_guess[0] ==
